9cfbb916e6ab38120229fad7bd3ec93dd9507f0a
kruceo <kruceo.com@gmail.com>
2023-12-07 09:41:58 -0300

    fix: rewrite homepage

--- src/components/builds/homepage/HomeSection1.tsx
+++ src/components/builds/homepage/HomeSection1.tsx
@@ -11,29 +11,30 @@ export default function HomeSection1() {
                 <Box>
                     <div className="icon" style={{ width: 120, height: 120, backgroundSize: 'contain', backgroundRepeat: 'no-repeat' }}>📦</div>
                     <h4>Open Source</h4>
-                    <p style={{ padding: 20, textAlign: 'center' }}>
-                        My open-source projects are available for inspiration and collaboration. Feel free to explore and benefit from the solutions I share with the community.
+                    <p style={{ padding: 20, textAlign: 'center' }}>     
+                        Feel free to explore and collaborate on the open-source projects. Your contributions and insights are valued as the projects continue to evolve. Join the community effort to enhance and refine these solutions, fostering a collaborative environment for innovation and shared knowledge.
                     </p>
                 </Box>
                 <Box>
                     <div className="icon" style={{ width: 120, height: 120, backgroundSize: 'contain', backgroundRepeat: 'no-repeat' }}>🔩</div>
                     <h4>Modularity</h4>
                     <p style={{ padding: 20, textAlign: 'center' }}>
-                        I value modularity, which allows for efficient and flexible organization of the entire project.
+                    See Kruceo's acknowledgment of modularity's value in efficiently organizing their projects, demonstrating dedication to scalable solutions that significantly contribute to project success.
                     </p>
                 </Box>
                 <Box>
                     <div className="icon" style={{ width: 120, height: 120, backgroundSize: 'contain', backgroundRepeat: 'no-repeat' }}>🗿</div>
                     <h4>Efficiency</h4>
                     <p style={{ padding: 20, textAlign: 'center' }}>
-                    I focus on efficient and intuitive solutions in my projects.
+                        Kruceo has efficient and intuitive solutions at the core of their projects, emphasizing a commitment to streamlined and user-friendly outcomes. 
                         </p>
                 </Box>
                 <Box>
                     <div className="icon" style={{ width: 120, height: 120, backgroundSize: 'contain', backgroundRepeat: 'no-repeat' }}>📚</div>
                     <h4>Continuous Learning</h4>
                     <p style={{ padding: 20, textAlign: 'center' }}>
-                    I'm committed to continuous learning, always striving to improve my skills and stay up-to-date with the latest trends and technologies.
+                    
+                        Kruceo values continuous learning, recognizing it as a cornerstone for personal and professional growth. This commitment underscores an ongoing dedication to staying informed, adapting to evolving trends, and refining skills. 
                         </p>
                 </Box>
 
