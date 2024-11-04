ef691365cb95be11ce38388244754c4eca093f2b
kruceo <kruceo.com@gmail.com>
2024-11-04 15:12:51 -0300

    fix: improve imports

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Error404 from "./components/builds/404/Error404";
 import Docs from "./components/builds/docs/Docs";
 import Blog from "./components/builds/blog/Blog";
 import Torneio from "./components/builds/torneio/Torneio";
-import TestingPhone from "./components/builds/torneio/TestingPhone";
 export default function App() {
 
   window.localStorage.getItem('dark-mode')??window.localStorage.setItem("dark-mode","1")
@@ -35,9 +34,6 @@ export default function App() {
       </Path>
       <Path as="/torneio-app" absolute>
         <Torneio/>
-      </Path>
-      <Path as="/torneio-app/testing" absolute>
-        <TestingPhone/>
       </Path>
       <E404>
         <Error404></Error404>
--- src/components/builds/torneio/Smartphone.tsx
+++ src/components/builds/torneio/Smartphone.tsx
@@ -1,6 +1,5 @@
 import "./Smartphone.less";
 import phoneImg from '../../../assets/PHONE.svg'
-import phoneMaskImg from '../../../assets/phone_mask.png'
 export default function (props: { screenImage: string }) {
     return <div className="smartphone">
         <div className="screen" style={{ }}>
