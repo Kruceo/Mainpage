cc895a79bcde3f5249149b98a7897d81d3a311f1
kruceo <kruceo.com@gmail.com>
2024-11-13 17:25:51 -0300

    update pnpm lockfile

--- src/components/builds/homepage/Welcome.tsx
+++ src/components/builds/homepage/Welcome.tsx
@@ -4,22 +4,6 @@ import "./Welcome.less"
 import WelcomeMainText from "./WelcomeMainText"
 interface Chunk { w: number, h: number, x: number, y: number, src: string }
 export default function Welcome() {
-    // return <Content className=" first"><iframe className="welcome" src="https://kruceo.com/wallpaper" frameBorder={0} style={{ width: "100%" }}>
-    //     <h2 style={{
-    //         position: "absolute",
-    //         left: 100,
-    //         color: 'white',
-    //         fontSize: 44,
-    //         fontWeight: 'bold',
-    //         textAlign: 'center'
-    //     }}>Any good<br /> <span style={{ fontSize: 84, color: 'white', }}>welcome</span></h2>
-    // </iframe>
-    // </Content>
-    // return <section className="welcome">
-    //     <div>
-    //         
-    //     </div>
-    // </section>
     const sec = useRef<HTMLDivElement>(null)
     const [chunks, setChunks] = useState<Chunk[]>([])
     function genChunks() {
