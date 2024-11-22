80353ff81607c184fdfab85caedd4689c5f85738
kruceo <kruceo.com@gmail.com>
2024-11-22 10:22:07 -0300

    fix: locales

--- src/components/builds/homepage/WelcomeMainText.tsx
+++ src/components/builds/homepage/WelcomeMainText.tsx
@@ -1,30 +1,38 @@
 import { useEffect, useState } from "react";
 import "./WelcomeMainText.less";
 import AButton from "../../essential/Button/AButton";
+import { locale } from "../../../lib/lang";
 
 export default function () {
-    const [index, setIndex] = useState(0)
-    const texts = ["creation", "Kruceo"]
-    useEffect(() => {
-        const handler = () => setIndex(index == (texts.length - 1) ? 0 : index + 1)
-        const interval = setInterval(handler, 3500)
-        return () => clearInterval(interval)
-    })
-    return <div className="wee" onClick={() => setIndex(index == (texts.length - 1) ? 0 : index + 1)}>
+    return <div className="wee">
         <h1>Kruceo</h1>
         <h2>
-            Creating to enhance code.<br />
-            Coding to enhance <span><SwitchableText texts={texts} index={index} /></span>.
+            {
+                swParseText(locale.welcome.subtitle).map(textPart => {
+                    if (typeof textPart == "object")
+                        return <span>{textPart.length >= 2 ? <SwitchableText texts={textPart} interval={6000} /> : textPart[0]}</span>
+                    return textPart
+                })
+            }
         </h2>
-        <p>Solutions in infrastructure, frontend, backend, <br />DevOps, design, and beyond.</p>
+        <p>{locale.welcome.text}</p>
         <div className="buttons-container">
             <AButton href="https://github.com/kruceo" ><i className="icon-github"></i> Github</AButton>
-            <AButton href="/projects"><i className="icon-cat_icon"></i> About</AButton>
+            <AButton href="/projects"><i className="icon-cat_icon"></i> {locale.about.title}</AButton>
         </div>
     </div>
 }
 
-function SwitchableText(props: { texts: string[], index: number }) {
+function swParseText(str: string) {
+    const matchs = str.match(/\{.+\}/)
+    if (!matchs) return [str]
+    const splited = str.split(matchs[0])
+    const parsedMatch = matchs[0].replace(/\{|\}/g, "").split(",")
+    const all = [splited[0], parsedMatch, splited[1]]
+    return all
+}
+
+export function SwitchableText(props: { texts: string[], interval?: number }) {
     const [sizes, setSizes] = useState<DOMRect[]>([])
     const longer = sizes.sort((a, b) => b.width - a.width).at(0)
     useEffect(() => {
@@ -33,10 +41,16 @@ function SwitchableText(props: { texts: string[], index: number }) {
         setSizes(items.map(e => e.clientRect))
     }, [])
 
-    const index = props.index
+    const [index, setIndex] = useState(0)
+
+    useEffect(() => {
+        const handler = () => setIndex(index == (props.texts.length - 1) ? 0 : index + 1)
+        const interval = setInterval(handler, props.interval ?? 3000)
+        return () => clearInterval(interval)
+    })
 
     return <span className="switchable-text" style={{ width: sizes.at(index)?.width, height: longer?.height }} >
-        <span className="roller" style={{ transform: `translate(0%,-${index * 100 / sizes.length}%)` }} >
+        <span className="roller" style={{ transform: `translate(0px,-${sizes.reduce((a, n, i) => a + ((i < index) ? n.height : 0), 0)}px)` }} >
             {props.texts.map((text, i) =>
                 <span key={i} className="item" id={`item-${i}`}>{text}</span>
             )}
