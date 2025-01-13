0568dcf3373aa26b7dc48e3d39aa79df498cc4de
kruceo <kruceo.com@gmail.com>
2025-01-13 10:42:37 -0300

    fix: remove testing form

--- src/components/builds/torneio/Torneio.tsx
+++ src/components/builds/torneio/Torneio.tsx
@@ -28,37 +28,11 @@ export default function () {
                     </ul>
 
 
-                    <DownloadOnGooglePlayButton href="#" classname="torneio-google-play-button" topText="Em breve no" />
+                    <DownloadOnGooglePlayButton href="https://play.google.com/store/apps/details?id=com.kruceo.torneio" classname="torneio-google-play-button" />
                 </div>
                 <Smartphone screenImage={torneioSSImg} />
             </div>
-            <Title>Participar do Teste</Title>
-            <div className="testing-content">
-                <p>Ter pessoas reais participando dos testes do App é essencial para sua evolução.<br />Além de ser um requisito obrigatório para o mesmo estar presente no Google Play Store. <br />Solicite acesso antecipado agora mesmo!</p>
-                <form className={sended ? "sended" : ""} onSubmit={(e) => {
-                    e.preventDefault()
-                    const data = new FormData(e.target as HTMLFormElement)
-                    fetch("https://torneio-testing-email.kruceo.com", {
-                        method: "POST",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify({
-                            email: data.get("email-input")?.toString().slice(0, 256),
-                            phone: data.get("phone-input")?.toString().slice(0, 32)
-                        })
-                    })
-                    setSended(true)
-                }}>
-                    <label htmlFor="email-input">Email*:</label>
-                    <input placeholder="example@mail.com" required name="email-input" id="email-input" type="email" />
-                    <label htmlFor="phone-input">Telefone (Opcional):</label>
-                    <input placeholder="98 7654 3210" name="phone-input" id="email-input" type="tel" />
-                    <Button disabled={sended}>Enviar</Button>
-                    {sended ?
-                    <p className="thanks">Obrigado pela Colaboração!</p>
-                    : null
-                }
-                </form>
-            </div>
+           
         </Content>
         <Footer></Footer>
     </>
